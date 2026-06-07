4380f8198a7c88510ef7566583fb167a65b32d37
paviraju-87 <paviraju@gmail.com>
2026-06-07 13:21:24 +0000

    updated commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+from huggingface_hub import hf_hub_download
+
+# Download the model from the Model Hub
+model_path = hf_hub_download(repo_id="PaviRaju/tourism-wellness-package-purchase", filename="best_tourism_model_v1.joblib")
+
+# Load the model
+model = joblib.load(model_path)
+
+# Streamlit UI for Customer Churn Prediction
+st.title("Tourism Wellness Package Customer Prediction App")
+st.write("The Tourism Wellness Package Customer App is an internal tool for travel company that predicts whether customers will buy a Wellness package.")
+st.write("Enter customer details to predict if they will purchase the Wellness Tourism Package.")
+
+# Numerical features
+age = st.slider("Age", 18, 70, 30)
+type_of_contact = st.selectbox("Type of Contact", ['Self Enquiry', 'Company Invited'])
+occupation = st.selectbox("Occupation", ['Salaried', 'Small Business', 'Large Business', 'Free Lancer'])
+product_pitched = st.selectbox("Product Pitched", ['Basic', 'Deluxe', 'Standard', 'Super Deluxe', 'King'])
+marital_status = st.selectbox("Marital Status", ['Married', 'Single', 'Divorced'])
+number_of_person_visiting = st.slider("Number Of Person Visiting", 1, 5, 2)
+number_of_children_visiting = st.slider("Number Of Children Visiting", 0, 5, 0)
+duration_of_pitch = st.slider("Duration Of Pitch (minutes)", 0, 60, 10)
+city_tier = st.selectbox("City Tier", [1, 2, 3])
+gender = st.selectbox("Gender", ['Male', 'Female', 'Fe Male']) # 'Fe Male' is a common typo in datasets
+preferred_property_star = st.selectbox("Preferred Property Star", [3.0, 4.0, 5.0])
+number_of_trips = st.slider("NumberOfTrips (annually)", 0, 20, 5)
+pitch_satisfaction_score = st.slider("Pitch Satisfaction Score (1-5)", 1, 5, 3)
+number_of_followups = st.slider("NumberOfFollowups", 0, 10, 3)
+passport = st.selectbox("Passport", [0, 1], format_func=lambda x: 'Yes' if x==1 else 'No')
+own_car = st.selectbox("Own Car", [0, 1], format_func=lambda x: 'Yes' if x==1 else 'No')
+
+# Convert categorical inputs to match model training
+input_data = pd.DataFrame([{
+        'Age': age,
+        'TypeofContact': type_of_contact,
+        'CityTier': city_tier,
+        'DurationOfPitch': duration_of_pitch,
+        'Occupation': occupation,
+        'Gender': gender,
+        'NumberOfPersonVisiting': number_of_person_visiting,
+        'NumberOfFollowups': number_of_followups,
+        'ProductPitched': product_pitched,
+        'PreferredPropertyStar': preferred_property_star,
+        'MaritalStatus': marital_status,
+        'NumberOfTrips': number_of_trips,
+        'Passport': passport,
+        'PitchSatisfactionScore': pitch_satisfaction_score,
+        'OwnCar': own_car,
+        'NumberOfChildrenVisiting': number_of_children_visiting,
+    }])
+
+# Predict button
+if st.button("Predict"):
+    prediction= model.predict(input_data)[0]
+    if prediction[0] == 1:
+            st.success(f"Prediction: This customer is LIKELY to purchase the package")
+    else:
+            st.info(f"Prediction: This customer is UNLIKELY to purchase the package")
---
+++ tourism_project/model_building/prep.py
@@ -0,0 +1,84 @@
+# for data manipulation
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi
+
+# Define constants for the dataset and output paths
+api = HfApi(token=os.getenv("HF_TOKEN"))
+datasetPath = "hf://datasets/PaviRaju/tourism-wellness-package-purchase/tourism.csv"
+tourismDataSet = pd.read_csv(datasetPath)
+print("Dataset loaded successfully.")
+
+# Print the shape of the dataset
+print(f"Shape of the cleaned dataset: {tourismDataSet.shape}")
+
+# Check for duplicates
+print("Number of duplicate rows:", tourismDataSet.duplicated().sum())
+
+# Remove duplicates
+tourismDataSet = tourismDataSet.drop_duplicates()
+
+# Check for null values
+print("Null values per column:\n", tourismDataSet.isnull().sum())
+
+# Handle missing values: Fill numerical NaNs with the mean and categorical NaNs with the mode.
+for column in tourismDataSet.columns:
+    if tourismDataSet[column].isnull().any():
+        if pd.api.types.is_numeric_dtype(tourismDataSet[column]):
+            # Fill numeric NaNs with mean
+            tourismDataSet[column] = tourismDataSet[column].fillna(tourismDataSet[column].mean())
+        else:
+            # Fill categorical NaNs with mode, fallback to 'Unknown'
+            mode_val = tourismDataSet[column].mode()[0] if not tourismDataSet[column].mode().empty else 'Unknown'
+            tourismDataSet[column] = tourismDataSet[column].fillna(mode_val)
+
+print(f"Shape of the cleaned dataset: {tourismDataSet.shape}")
+print("Missing values after cleaning:\n",tourismDataSet.isnull().sum())
+
+# Treat the Gender column
+# Replace "Fe Male" with "Female" in the Gender column
+tourismDataSet['Gender'] = tourismDataSet['Gender'].replace("Fe Male", "Female")
+print(tourismDataSet['Gender'].unique())
+
+# Treat the Marital Status Columns
+# Replace "Unmarried" with "Single" in MaritalStatus column
+tourismDataSet['MaritalStatus'] = tourismDataSet['MaritalStatus'].replace("Unmarried", "Single")
+print(tourismDataSet['MaritalStatus'].unique())
+
+# Define the target variable for the classification task
+target = 'ProdTaken'
+
+# Define predictor matrix (X) using selected numeric and categorical features
+X = tourismDataSet.drop(columns=[target])
+
+# Define target variable
+y = tourismDataSet[target]
+
+# Split dataset into train and test
+# Split the dataset into training and test sets
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y,              # Predictors (X) and target variable (y)
+    test_size=0.2,     # 20% of the data is reserved for testing
+    random_state=42    # Ensures reproducibility by setting a fixed random seed
+)
+
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id="PaviRaju/tourism-wellness-package-purchase",
+        repo_type="dataset",
+    )
